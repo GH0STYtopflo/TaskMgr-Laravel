b3a1eb4b1f432b8ddf3aaa8cb4caafed8d2c5ddd
Sepehr <soapss85@proton.me>
2026-09-23 19:11:26 +0330

    feat: Add admin dashboard

--- resources/views/users/admin/dashboard.blade.php
+++ resources/views/users/admin/dashboard.blade.php
@@ -1,3 +1,20 @@
-<x-layout title="Dashboard">
+<x-layout title="Admin Dashboard">
+    <label class="text-white font-bold text-3xl mx-auto mt-10">Places</label>
+    <div class="grid grid-cols-4 w-1/2 mx-auto mt-5 gap-2">
+        <a href="/users" class="w-full p-4 rounded" style="background-color: #08032a">
+            <label class="text-white font-bold">Users</label>
+        </a>
 
+        <a href="/tasks" class="w-full p-4 rounded" style="background-color: #08032a">
+            <label class="text-white font-bold">Tasks</label>
+        </a>
+
+        <a href="/categories" class="w-full p-4 rounded" style="background-color: #08032a">
+            <label class="text-white font-bold">Categories</label>
+        </a>
+
+        <a href="/comments" class="w-full p-4 rounded" style="background-color: #08032a">
+            <label class="text-white font-bold">Comments</label>
+        </a>
+    </div>
 </x-layout>
