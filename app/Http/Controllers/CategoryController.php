2ef83a82759a8e8ddfeb9b10de690d3465524e34
Sepehr <soapss85@proton.me>
2026-09-24 12:38:38 +0330

    fix: Fix the Create Category form request for updating categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,13 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateCategoryRequest;
 use App\Models\Category;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $categories = Category::all();
@@ -18,41 +14,29 @@ class CategoryController extends Controller
         return view('categories.index', ['categories' => $categories]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(CreateCategoryRequest $request)
     {
         Category::create([
             'title' => $request->title,
         ]);
 
-        return redirect('/categories');
+        return redirect()->route('categories.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Category $category)
     {
         return view('categories.show', ['category' => $category]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(CreateCategoryRequest $request, Category $category)
     {
         $category->update([
             'title' => $request->title,
         ]);
 
-        return redirect('/categories/' . $category->id);
+        return redirect()->route('categories.show', ['category' => $category]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Category $category)
     {
         $category->delete();
